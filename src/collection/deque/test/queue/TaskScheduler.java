630d68639152acb28634785c76ad65091e5070f9
una <una_j@naver.com>
2024-10-03 22:28:17 +0900

    ~ 세션 12 - 끝

---
+++ src/collection/deque/test/queue/TaskScheduler.java
@@ -0,0 +1,24 @@
+package collection.deque.test.queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class TaskScheduler {
+
+    private Deque<Task> tasks = new ArrayDeque<>();
+
+    public void addTask(Task task) {
+        tasks.offer(task);
+    }
+
+    public void processNextTask() {
+        Task task = tasks.poll();
+        if (task != null) {
+            task.execute();
+        }
+    }
+
+    public int getRemainingTasks() {
+        return tasks.size();
+    }
+}
